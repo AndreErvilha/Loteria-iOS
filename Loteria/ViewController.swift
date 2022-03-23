3b6097e48791ba155faf06f4f8c7366a1581b2a1
andreervilha <andre.ervilha@cwi.com.br>
2022-03-22 21:51:10 -0300

    Initial Commit

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Andre Eduardo Ribeiro Ervilha on 22/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
