5e0e6f36242564891a8e18ca4c4608c3e2c5ec5b
andreervilha <andre.ervilha@cwi.com.br>
2022-03-30 08:52:39 -0300

    :feat: Created code to generate game

--- Loteria/ViewController.swift
+++ Loteria/ViewController.swift
@@ -6,6 +6,12 @@
 //
 
 import UIKit
+import Foundation
+
+enum GameType: String {
+    case megasena = "Mega-Sena"
+    case quina = "Quina"
+}
 
 class ViewController: UIViewController {
 
@@ -15,11 +21,51 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        showNumber(for: .megasena)
+    }
+    
+    func showNumber(for type: GameType){
+        lbGameType.text = type.rawValue
+        
+        var game: [Int] = []
+        switch type{
+            case .megasena:
+                game = randomList(total: 6, universe: 60)
+                balls.last!.alpha = 1
+                break
+            case .quina:
+                game = randomList(total: 5, universe: 80)
+                balls.last!.alpha = 0
+                break
+        }
+        
+        for (index, game) in game.enumerated(){
+            balls[index].setTitle("\(game)", for: .normal)
+        }
     }
 
     @IBAction func generateGame() {
+        switch scGameType.selectedSegmentIndex {
+            case 0:
+                showNumber(for: .megasena)
+            default:
+                showNumber(for: .quina)
+        }
     }
     
+    func randomList(total: Int, universe: Int) -> [Int] {
+        var result : [Int] = [];
+        
+        while result.count < 6 {
+            let randomNumber = Int(arc4random_uniform(UInt32(universe))+1)
+            
+            if !result.contains(randomNumber) {
+                result.append(randomNumber)
+            }
+        }
+        
+        return result.sorted()
+    }
+     
 }
 
